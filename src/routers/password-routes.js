d605ae68d7787314c9998feca2719309b6c31623
Ren666nvs <164177806+Ren666nvs@users.noreply.github.com>
2025-03-25 12:32:00 +0800

    a

--- src/routers/password-routes.js
+++ src/routers/password-routes.js
@@ -1,6 +1,6 @@
-import express from "express";
-import { sendPasswordResetEmail } from "../controller/passwordreset/passwordreset.js";
+// import express from "express";
+// import { sendPasswordResetEmail } from "../controller/passwordreset/passwordreset.js";
 
-export const passwordRouter = express.Router();
+// export const passwordRouter = express.Router();
 
-passwordRouter.post("/", sendPasswordResetEmail);
+// passwordRouter.post("/", sendPasswordResetEmail);
